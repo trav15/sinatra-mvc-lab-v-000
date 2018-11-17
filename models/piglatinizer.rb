11ca8e82c1a25dc5367984f2528f2eaaada33b34
Travis T <travistokuyama@gmail.com>
2018-11-17 19:45:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,16 @@
 class PigLatinizer
   attr_reader :text
 
-  def initialize(text=nil)
-    @text = text
+  def initialize(text)
+    @text = text.downcase
   end
 
-  def piglatinize(text)
+  def piglatinizer
     words = @text.split(" ")
     words.each do |word|
       word << "#{word.chars.first}ay"
       word.slice!(0)
     end
-    words.join(" ")
+    words.joing(" ")
   end
 end
