e08dea5123f30f10400ac77c9aead69ac2347c9c
Travis T <travistokuyama@gmail.com>
2018-11-17 19:27:16 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,12 @@ class PigLatinizer
   attr_reader :text
 
   def initialize(text)
-    @text = text
+    @text = text.downcase
+  end
+
+  def piglatinizer
+    words = @text.split(" ")
+    words.each do |word|
+      word << "#{ay"
   end
 end
